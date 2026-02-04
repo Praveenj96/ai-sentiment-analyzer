ef26b5a21669e3db16957337539a472607fe8886
Praveenj96 <109998471+Praveenj96@users.noreply.github.com>
2026-02-04 14:32:06 +0530

    Add sentiment analyzer application files

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from textblob import TextBlob
+
+st.title("AI Sentiment Analyzer")
+
+text = st.text_area("Enter text")
+
+if st.button("Analyze"):
+    if text.strip() == "":
+        st.warning("Please enter text")
+    else:
+        polarity = TextBlob(text).sentiment.polarity
+
+        if polarity > 0:
+            st.success("Positive 😊")
+        elif polarity < 0:
+            st.error("Negative 😠")
+        else:
+            st.info("Neutral 😐")
+            
+        st.write("Score:", polarity)
